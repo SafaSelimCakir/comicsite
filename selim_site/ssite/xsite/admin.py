33ad197954126df2cee7ff40d1fd6fae0ee602fd
kayrahancoskun <safa.selim00@gmail.com>
2024-10-30 17:21:36 +0300

    init

--- selim_site/ssite/xsite/admin.py
+++ selim_site/ssite/xsite/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import xsite, Category
-
 from .models import *
 
 admin.site.register(Customer)
--- selim_site/ssite/xsite/urls.py
+++ selim_site/ssite/xsite/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 from xsite import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 #http://127.0.0.1:8000/user             =>homepage
 #http://127.0.0.1:8000/user/index        =>homepage
 #http://127.0.0.1:8000/user/blogs        =>blogs
@@ -17,3 +18,4 @@ urlpatterns = [
 
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
