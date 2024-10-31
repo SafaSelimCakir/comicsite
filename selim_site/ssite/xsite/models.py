3e4fbbfb391f1d79a8b74d5e720c8c73b00f6592
kayrahancoskun <safa.selim00@gmail.com>
2024-10-31 17:26:15 +0300

    init

--- selim_site/ssite/xsite/models.py
+++ selim_site/ssite/xsite/models.py
@@ -12,20 +12,25 @@ class Customer(models.Model):
 		return self.name
 
 
-class Product(models.Model):
-	name = models.CharField(max_length=200)
-	price = models.FloatField()
-	digital = models.BooleanField(default=False,null=True, blank=True)
+from django.db import models
 
-	def __str__(self):
-		return self.name
-	@property
-	def imageURL(self):
-		try:
-			url = self.image.url
-		except:
-			url = ''
-		return url
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=True)
+    image = models.ImageField(upload_to='img/', null=True, blank=True)  # Resim alanı eklendi
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+
 
 class Order(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
@@ -59,6 +64,7 @@ class ShippingAddress(models.Model):
 class xsite(models.Model):
     # model alanları
     pass
+
 
 class Category(models.Model):
     # model alanları
--- selim_site/ssite/xsite/views.py
+++ selim_site/ssite/xsite/views.py
@@ -1,18 +1,17 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from .models import *
-
-
-def home(request):
-    products = Product.objects.all()
-    context={'products':products}
-    return render(request, 'xsite/home.html',context)
+from .models import Product
 
 def cart(request):
-    context={}
-    return render(request, 'xsite/cart.html',context)
+    products = Product.objects.all()  # Tüm ürünleri al
+    context = {'products': products}
+    return render(request, 'xsite/cart.html', context)
+
+def home(request):
+    context = {}
+    return render(request, 'xsite/home.html', context)
 
 def checkout(request):
-    context={}
-    return render(request, 'xsite/checkout.html',context)
-
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'xsite/checkout.html', context)
